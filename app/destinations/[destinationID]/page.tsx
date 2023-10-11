2da13367c5114165660dfa94e127fb57f9cde612
Malidudle <132945899+Malidudle@users.noreply.github.com>
2023-10-11 20:44:26 +0100

    Added navbar to destination page

--- app/destinations/[destinationID]/page.tsx
+++ app/destinations/[destinationID]/page.tsx
@@ -1,3 +1,4 @@
+import Navbar from "@/components/navbar/Navbar";
 import { prisma } from "@/lib/prisma";
 import Image from "next/image";
 
@@ -16,28 +17,31 @@ const page = async ({
     });
 
     return (
-      <main className="min-h-[100vh] flex items-center">
-        <div className="ml-auto mr-auto flex flex-col items-center gap-5 lg:flex-row md:flex-col sm:flex-col">
-          {destination?.image_url ? (
-            <Image
-              src={destination?.image_url!}
-              alt={destination?.Description!}
-              width={300}
-              height={300}
-              placeholder="blur"
-              blurDataURL="https://images.pexels.com/photos/1526/dark-blur-blurred-gradient.jpg?cs=srgb&dl=pexels-splitshire-1526.jpg&fm=jpg"
-              loading="eager"
-              className="rounded-2xl"
-            />
-          ) : (
-            <p>image not found</p>
-          )}
-          <div className="flex flex-col gap-2 text-center">
-            <h1 className="text-4xl">{destination?.Name}</h1>
-            <p className="text-sm">{destination?.Description}</p>
+      <>
+      <Navbar />
+        <main className="min-h-[100vh] flex items-center p-2">
+          <div className="ml-auto mr-auto flex flex-col items-center gap-5 lg:flex-row md:flex-col sm:flex-col">
+            {destination?.image_url ? (
+              <Image
+                src={destination?.image_url!}
+                alt={destination?.Description!}
+                width={300}
+                height={300}
+                placeholder="blur"
+                blurDataURL="https://images.pexels.com/photos/1526/dark-blur-blurred-gradient.jpg?cs=srgb&dl=pexels-splitshire-1526.jpg&fm=jpg"
+                loading="eager"
+                className="rounded-2xl"
+              />
+            ) : (
+              <p>image not found</p>
+            )}
+            <div className="flex flex-col gap-2 text-center">
+              <h1 className="text-4xl">{destination?.Name}</h1>
+              <p className="text-sm">{destination?.Description}</p>
+            </div>
           </div>
-        </div>
-      </main>
+        </main>
+      </>
     );
   } catch (error) {
     console.error(error);
