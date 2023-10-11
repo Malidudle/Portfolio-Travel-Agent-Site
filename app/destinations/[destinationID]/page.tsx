db9cb448c834df120342237836413890a8f0d2de
Malidudle <132945899+Malidudle@users.noreply.github.com>
2023-10-11 20:13:47 +0100

    slug page

--- app/destinations/[destinationID]/page.tsx
+++ app/destinations/[destinationID]/page.tsx
@@ -1,5 +1,5 @@
 import { prisma } from "@/lib/prisma";
-import React from "react";
+import Image from "next/image";
 
 const page = async ({
   params: { destinationID },
@@ -9,19 +9,41 @@ const page = async ({
   };
 }) => {
   try {
-    console.log(destinationID);
     const destination = await prisma.destinations.findUnique({
       where: {
         id: destinationID,
       },
     });
 
-    return <div>{destination?.Name}</div>;
+    return (
+      <main className="min-h-[100vh] flex items-center">
+        <div className="ml-auto mr-auto flex flex-col items-center gap-5 lg:flex-row md:flex-col sm:flex-col">
+          {destination?.image_url ? (
+            <Image
+              src={destination?.image_url!}
+              alt={destination?.Description!}
+              width={300}
+              height={300}
+              placeholder="blur"
+              blurDataURL="https://images.pexels.com/photos/1526/dark-blur-blurred-gradient.jpg?cs=srgb&dl=pexels-splitshire-1526.jpg&fm=jpg"
+              loading="eager"
+              className="rounded-2xl"
+            />
+          ) : (
+            <p>image not found</p>
+          )}
+          <div className="flex flex-col gap-2 text-center">
+            <h1 className="text-4xl">{destination?.Name}</h1>
+            <p className="text-sm">{destination?.Description}</p>
+          </div>
+        </div>
+      </main>
+    );
   } catch (error) {
     console.error(error);
 
     return (
-      <main>
+      <main className="min-h-[100vh] flex items-center">
         <p>{"Destination not found :("}</p>
       </main>
     );
